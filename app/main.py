005a295f43fb6cacba4f43ea1a621e19d4a04387
jpwernowski <patrick.wernowski@digicult-verbund.de>
2026-07-07 22:37:29 +0200

    Changed title and version to variables

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@
 from fastapi import FastAPI
 
 from api.routes import router
+from core.config import APP_NAME, APP_VERSION
 
 app = FastAPI(
-    title="Image Similarity SEARCH API",
+    title=APP_NAME,
     description="A cloud-native image similarity search API using FastAPI, CLIP, Qdrant and Kubernetes.",
-    version="0.1.0",
+    version=APP_VERSION,
 )
 
 @app.get("/")
