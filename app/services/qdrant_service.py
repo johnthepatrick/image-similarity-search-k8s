0bfecddd49cd6c0855d0b7b5116040584ab7d83f
jpwernowski <johnpatrickwernowski@googlemail.com>
2026-07-14 22:51:03 +0200

    Added create_qdrant_collection function

--- app/services/qdrant_service.py
+++ app/services/qdrant_service.py
@@ -1,11 +1,18 @@
 import os
 import requests
 
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
 
 QDRANT_HOST = os.getenv("QDRANT_HOST", "localhost")
 QDRANT_PORT = os.getenv("QDRANT_PORT", "6333")
 QDRANT_HEALTH_URL = f"http://{QDRANT_HOST}:{QDRANT_PORT}/healthz"
 
+client = QdrantClient(
+    host=QDRANT_HOST,
+    port=QDRANT_PORT
+    )
 
 def check_qdrant_health() -> bool:
     try:
@@ -14,4 +21,27 @@ def check_qdrant_health() -> bool:
         return True
     except requests.RequestException as error:
         print(f"Qdrant health check failed: {error}")
+        return False
+    
+def create_qdrant_collection() -> bool:
+    collection_name = "image_vectors"
+
+    try:
+        if client.collection_exists(collection_name):
+            print(f"Qdrant collection '{collection_name}' already exists")
+            return True
+
+        client.create_collection(
+            collection_name=collection_name,
+            vectors_config=VectorParams(
+                size=512,
+                distance=Distance.DOT,
+            ),
+        )
+
+        print(f"Qdrant collection '{collection_name}' created")
+        return True
+
+    except Exception as error:
+        print(f"Failed to create Qdrant collection: {error}")
         return False
